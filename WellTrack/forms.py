825b8897e19ee1c7908dcae0c90e660181045562
Ekwenye Joseph <130204527+Joseph22-sys@users.noreply.github.com>
2025-04-05 12:46:22 +0100

    Delete WellTrack directory

--- WellTrack/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,PasswordField
-
-class Login():
-    name = StringField("Name")
-    password = PasswordField("password")
-    submit = SubmitField("Submit")
